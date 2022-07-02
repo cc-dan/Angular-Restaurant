be0ea1d22053a564f7959671a6310377ca905d61
Dante <dantecocciolo@gmail.com>
2022-07-01 23:39:56 -0300

    login: elementos de UX, validación y manejo de tokens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import AppRoutingModule from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,10 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import Swal from 'sweetalert2'
+import LoginService from '../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
-  email: string;
-  password: string;
+  emailFormInvalid?:Boolean
+  passwordFormInvalid?:Boolean
+  sendRequestPossible:Boolean // Deshabilitar botón de login
 
-  constructor() {}
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.compose([Validators.email, Validators.required])),
+    password: new FormControl('', Validators.required)
+  })
 
-  validateLogin():Boolean {
-    console.log('validate login', this.email, this.password)
-    return true
+  constructor(public loginService:LoginService) { // Inyectamos el servicio de login --> Nos podemos referir a él mediante "this"
+    this.sendRequestPossible = true 
+  } 
+
+  validateLoginForm():Boolean|any {
+    this.emailFormInvalid = this.loginForm.get('email')?.invalid
+    this.passwordFormInvalid = this.loginForm.get('password')?.invalid
+
+    return !this.emailFormInvalid && !this.passwordFormInvalid
+  }
+
+  validateLoginData() {
+    if (this.validateLoginForm()) {
+      const formUserData = { 
+        email:this.loginForm.get('email')?.value, 
+        password:this.loginForm.get('password')?.value 
+      }
+
+      this.sendRequestPossible = false
+      
+      // Petición POST
+      this.loginService.validateLogin(formUserData).subscribe(
+        data => this.loginService.saveToken(data),                                  // Almacenar token
+        err => { console.log("ERROR: ", err); Swal.fire("Error de verificación") }, // Manejo de errores
+        () => console.log("Request verificada en los servidores de Alkemy")         // Finalización exitosa
+      ).add(
+        () => this.sendRequestPossible = true                                       // Función adicional ejecutada al final --> Cambiar el estado del botón login
+      ) 
+    } else { console.log("Los campos de email y contraseña no son válidos") }
   }
 }
